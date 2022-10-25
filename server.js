b04488f0e6c43030b6027368143a5de413e3c855
Emily Hill <emhillmed2021@gmail.com>
2022-10-25 13:27:05 -0500

    keep getting error message when trying to manipulate candidates table

--- server.js
+++ server.js
@@ -1,18 +1,63 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001; 
+const mysql = require('mysql2'); 
 const app = express(); 
 
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); 
 
+//connect to database 
+const db = mysql.createConnection({
+    host: 'localhost',
+    // MySQL username
+    user: 'root',
+    password: '',
+    database: 'election'
+}, 
+console.log('Connected to the election database.')
+); 
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    if(err){ 
+        console.log(err); 
+    }
+    console.log(rows);
+  });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row); 
+// })
+
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
+
+//create a candidate 
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?, ?, ?, ?)`; 
+const params = [8, 'Montague', 'Summers', 1]
+
+db.query(sql, params, (err, result) => {
+    if(err) {
+        console.log(err); 
+    }
+    console.log(result);
+});
 
 
 //Default response for any othe requst (Not Found)
 // needs to be at bottom or else will override other routes 
 app.use((req,res) => {
     res.status(404).end(); 
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
